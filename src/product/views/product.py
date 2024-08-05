6ac977ec0ddd9dcce163fe8a928da8c5499e0eda
Hadayetullah <hadayetullah30@gmail.com>
2024-08-06 01:26:43 +0600

    Show product variant data dynamically and group by product variant

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,7 +1,8 @@
 from django.views import generic
+from django.db.models import Q
 from django.core.paginator import Paginator
 
-from product.models import Variant, Product, ProductVariantPrice
+from product.models import Variant, Product, ProductVariantPrice, ProductVariant
 
 
 class CreateProductView(generic.TemplateView):
@@ -24,7 +25,6 @@ class ProductListView(generic.ListView):
 
     def get_queryset(self):
         queryset = Product.objects.all()
-
         title = self.request.GET.get('title', None)
         variant = self.request.GET.get('variant', None)
         price_from = self.request.GET.get('price_from', None)
@@ -34,11 +34,18 @@ class ProductListView(generic.ListView):
         if title:
             queryset = queryset.filter(title__icontains=title)
         if variant:
-            queryset = queryset.filter(productvariantprice__product_variant_one__variant_title=variant)
-        if price_from:
-            queryset = queryset.filter(productvariantprice__price__gte=float(price_from))
-        if price_to:
-            queryset = queryset.filter(productvariantprice__price__lte=float(price_to))
+            queryset = queryset.filter(
+                Q(productvariantprice__product_variant_one__variant_title=variant) |
+                Q(productvariantprice__product_variant_two__variant_title=variant) |
+                Q(productvariantprice__product_variant_three__variant_title=variant)
+            )
+        if price_from or price_to:
+            price_filter = Q()
+            if price_from:
+                price_filter &= Q(productvariantprice__price__gte=float(price_from))
+            if price_to:
+                price_filter &= Q(productvariantprice__price__lte=float(price_to))
+            queryset = queryset.filter(price_filter)
         if date:
             queryset = queryset.filter(created_at__date=date)
 
@@ -46,9 +53,25 @@ class ProductListView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        variants = ProductVariantPrice.objects.values_list('product_variant_one__variant_title', flat=True).distinct()
+        variants = ProductVariant.objects.values_list('variant_title', flat=True).distinct()
+        print("Variant: ", variants)
         context['variants'] = variants
-        
+
+        # a = Variant.objects.values_list('title', flat=True).distinct()
+        # for b in a:
+        #     c = ProductVariant.objects.filter(variant__title=b).values_list('variant_title', flat=True).distinct()
+        #     print(c)
+            # for d in c:
+            #     print("Variant title: ", d)
+
+        # Group product variants by variant titles
+        variant_groups = {}
+        for variant in Variant.objects.values_list('title', flat=True).distinct():
+            variant_groups[variant] = ProductVariant.objects.filter(variant__title=variant).values_list('variant_title', flat=True).distinct()
+        # print("Group: ", variant_groups)
+
+        context['variant_groups'] = variant_groups
+
         paginator = Paginator(self.get_queryset(), self.paginate_by)
         page = self.request.GET.get('page')
         products = paginator.get_page(page)
