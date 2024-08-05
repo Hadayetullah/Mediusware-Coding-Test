9b72093cde799a6aca8d46612a9eea9f38c81a3e
Hadayetullah <hadayetullah30@gmail.com>
2024-08-05 06:09:19 +0600

    Make a product filter with product title, product variant, price range and date.

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.core.paginator import Paginator
 
 from product.models import Variant, Product, ProductVariantPrice
 
@@ -16,64 +17,41 @@ class CreateProductView(generic.TemplateView):
     
 
 class ProductListView(generic.ListView):
+    model = Product
     template_name = 'products/list.html'
     context_object_name = 'products'
-    paginate_by = 10
+    paginate_by = 10  # Number of products per page
 
     def get_queryset(self):
         queryset = Product.objects.all()
 
-        # Apply filters if any
-        title = self.request.GET.get('title')
-        variant = self.request.GET.get('variant')
-        price_from = self.request.GET.get('price_from')
-        price_to = self.request.GET.get('price_to')
-        date = self.request.GET.get('date')
+        title = self.request.GET.get('title', None)
+        variant = self.request.GET.get('variant', None)
+        price_from = self.request.GET.get('price_from', None)
+        price_to = self.request.GET.get('price_to', None)
+        date = self.request.GET.get('date', None)
 
         if title:
-            queryset = queryset.filter(title__icontains(title))
+            queryset = queryset.filter(title__icontains=title)
         if variant:
-            queryset = queryset.filter(productvariantprice__product_variant_one__variant_title__icontains(variant) |
-                                       productvariantprice__product_variant_two__variant_title__icontains(variant) |
-                                       productvariantprice__product_variant_three__variant_title__icontains(variant)).distinct()
+            queryset = queryset.filter(productvariantprice__product_variant_one__variant_title=variant)
         if price_from:
-            queryset = queryset.filter(productvariantprice__price__gte=price_from)
+            queryset = queryset.filter(productvariantprice__price__gte=float(price_from))
         if price_to:
-            queryset = queryset.filter(productvariantprice__price__lte=price_to)
+            queryset = queryset.filter(productvariantprice__price__lte=float(price_to))
         if date:
             queryset = queryset.filter(created_at__date=date)
 
-        return queryset
+        return queryset.distinct()
 
     def get_context_data(self, **kwargs):
-        context = super(ProductListView, self).get_context_data(**kwargs)
-        products = self.get_queryset()
-        product_list = []
+        context = super().get_context_data(**kwargs)
+        variants = ProductVariantPrice.objects.values_list('product_variant_one__variant_title', flat=True).distinct()
+        context['variants'] = variants
+        
+        paginator = Paginator(self.get_queryset(), self.paginate_by)
+        page = self.request.GET.get('page')
+        products = paginator.get_page(page)
 
-        for product in products:
-            variants = ProductVariantPrice.objects.filter(product=product)
-            variant_list = []
-
-            for variant in variants:
-                variant_data = {
-                    'variant_title_one': variant.product_variant_one.variant_title if variant.product_variant_one else None,
-                    'variant_title_two': variant.product_variant_two.variant_title if variant.product_variant_two else None,
-                    'variant_title_three': variant.product_variant_three.variant_title if variant.product_variant_three else None,
-                    'price': variant.price,
-                    'stock': variant.stock,
-                }
-                variant_list.append(variant_data)
-
-            product_data = {
-                'title': product.title,
-                'description': product.description,
-                'created_at': product.created_at,
-                'variants': variant_list,
-            }
-            product_list.append(product_data)
-
-        context['product'] = True
-        context['products'] = product_list
-
-        # print(context)
+        context['products'] = products
         return context
